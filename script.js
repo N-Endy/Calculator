b48be6cd4f6902f0c97a9aab31b463a7670f2b29
N-Endy <okafornel@gmail.com>
2021-05-12 11:49:12 +0100

    modified on and off button function to disable other buttons when off

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Operations
 function add(a, b) {
     return a + b;
 }
@@ -14,9 +15,15 @@ function divide(a, b) {
     return a / b;
 }
 
+function power(a, b) {
+    return Math.pow(a, b);
+}
+
 function operate(operator, a, b) {
     operator(a, b);
 }
+
+
 
 // Set time on Calculator
 const time = document.querySelector('.time');
@@ -25,22 +32,113 @@ let minute = new Date().getMinutes();
 time.textContent = `${hour}:${minute}`
 
 
-// Numbers
+// Cache DOM of Numbers and Screens
 const numbers = document.querySelectorAll('.number');
 const problem = document.querySelector('.problem');
 const solution = document.querySelector('.solution');
-
+let num = "";
 
 // Display Numbers On Screen
 function showNumbers() {
-    let num = "";
+    
     numbers.forEach(number => {
         number.addEventListener('click', () => {
             num += number.textContent.toString();
-            problem.innerHTML = `<p>${Number(num)}</p>`
-            return problem.textContent;
+            problem.textContent = Number(num);
+            getNumber(problem.textContent);
         });
     });
 }
-
 showNumbers();
+
+// Function get digit
+function getNumber(num) {
+    return num;
+}
+
+function calculate() {
+    showNumbers();
+}
+
+// Operator Dom and Function
+const operators = document.querySelectorAll('.operator');
+
+function getOperator() {
+    let operatorSign = "";
+    operators.forEach(operator => {
+        operator.addEventListener('click', () => {
+            operatorSign = operator.textContent;
+            problem.textContent += ` ${operatorSign} `;
+        })
+    })
+}
+getOperator();
+
+// Function Clear Screen
+const clear = document.querySelector('.clear');
+function clearSCreen() {
+    problem.innerText = 0;
+    solution.textContent = 0;
+    num = "";
+}
+
+clear.addEventListener('click', () => {
+    clearSCreen();
+})
+
+// Function Delete
+const del = document.querySelector('.delete');
+
+function deleteNumber(digit) {
+    digit.toString();
+    let array = [...digit];
+    array.pop();
+}
+
+del.addEventListener('click', () => {
+    let num = getNumber();
+    deleteNumber(num);
+})
+
+
+
+// Disable Buttons
+const buttons = document.querySelectorAll('.button');
+
+function disableButtons() {
+    buttons.forEach(button => {
+        button.disabled = true;
+    })
+}
+
+// Enable Buttons
+function enableButtons() {
+    buttons.forEach(button => {
+        button.disabled = false;
+    })
+}
+
+
+// On and Off
+const on = document.querySelector('.on');
+function onAndOff() {
+    on.classList.toggle("green");
+    num = "";
+}
+
+on.addEventListener('click', () => {
+    onAndOff();
+    let onClass = on.className;
+    let name = onClass.slice(onClass.length -5);
+        if(name !== "green") {
+            disableButtons();
+            problem.innerText = "";
+            solution.textContent = "";
+            
+        } else {
+            enableButtons();
+            problem.innerText = 0;
+            solution.textContent = 0;
+        }
+    
+})
