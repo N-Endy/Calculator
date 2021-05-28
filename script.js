08e36842fc484447ca8bfd4277d2f033796d6da5
N-Endy <okafornel@gmail.com>
2021-05-28 16:17:37 +0100

    Really struggling here. Just adjusted displayValue function

--- script.js
+++ script.js
@@ -34,70 +34,40 @@ time.textContent = `${hour}:${minute}`
 
 // Cache DOM of Numbers and Screens
 const numbers = document.querySelectorAll('.number');
-const problem = document.querySelector('.problem');
-const problemScreen = problem.textContent;
+const display = document.querySelector('.problem');
 const solution = document.querySelector('.solution');
-let firstNum = "";
-let secondNum = "";
 
 // Display Numbers On Screen
-function showFirstNumbers() {
-    
+function displayValue() {
     numbers.forEach(number => {
         number.addEventListener('click', () => {
-            firstNum = number.textContent.toString();
-            problemScreen = Number(firstNum);
-            getFirstNumbers(problem.textContent);
-        });
-    });
-}
-showFirstNumbers();
+            const value = display.textContent;
 
-function showSecondNumbers() {
-    
-    numbers.forEach(number => {
-        number.addEventListener('click', () => {
-            secondNum = number.textContent.toString();
-            problem.textContent += Number(secondNum);
-            getSecondNumbers(problem.textContent);
-        });
-    });
+            if (display.textContent == '0') {
+                display.textContent = number.textContent;
+            } else {
+                display.textContent = value + number.textContent;
+            }
+        })
+    })
 }
-showSecondNumbers();
+displayValue();
 
-// Function get digit
-function getFirstNumbers(num) {
-    return num;
-}
 
-function getSecondNumbers(num) {
-    return num;
-}
-
-function calculate() {
-    showNumbers();
-}
 
 // Operator Dom and Function
 const operators = document.querySelectorAll('.operator');
 
 function getOperator() {
-    let operatorSign = "";
-    operators.forEach(operator => {
-        operator.addEventListener('click', () => {
-            operatorSign = operator.textContent;
-            problem.textContent += ` ${operatorSign} `;
-        })
-    })
+    
 }
 getOperator();
 
 // Function Clear Screen
 const clear = document.querySelector('.clear');
 function clearSCreen() {
-    problem.innerText = 0;
+    display.innerText = '0';
     solution.textContent = 0;
-    firstNum = "";
 }
 
 clear.addEventListener('click', () => {
@@ -141,8 +111,6 @@ function enableButtons() {
 const on = document.querySelector('.on');
 function onAndOff() {
     on.classList.toggle("green");
-    firstNum = "";
-    secondNum = "";
 }
 
 on.addEventListener('click', () => {
@@ -156,7 +124,7 @@ on.addEventListener('click', () => {
             
         } else {
             enableButtons();
-            problem.innerText = 0;
+            display.innerText = '0';
             solution.textContent = 0;
         }
     
